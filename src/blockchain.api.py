58f415aef014c906facff5017d96e84b33509b72
Divyesh Bhartiya <divyesh.bhartiya@gmail.com>
2021-05-27 17:50:15 +0530

    Blockchain
    Added is_valid endpoint to check the validity of the blockchain.

--- src/blockchain.api.py
+++ src/blockchain.api.py
@@ -43,4 +43,15 @@ def get_chain():
     return jsonify(response), 200
 
 
+# Checking the validity of Blockchain
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_valid_chain(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Something went HayWire. The Blockchain is not valid.'}
+        
+    return jsonify(response), 200
+
 app.run(host = '0.0.0.0', port = 5000)
